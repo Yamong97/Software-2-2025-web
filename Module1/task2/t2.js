ad385397c17bc8a78a9e0e198946871f461eb3ef
yamongphoo <yamongphoo012@gmail.com>
2025-11-04 16:08:02 +0630

    update

--- Module1/task2/t2.js
+++ Module1/task2/t2.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const name = prompt("Please enter name")
+const name = prompt('Please enter name');
 
-const greeting = `Hello, ${name}!!!!!`
+const greeting = `Hello, ${name}!!!!!`;
 
 // console.log(greeting);
 
-document.querySelector("#target"). innerHTML= greeting
+document.querySelector('#target').innerHTML = greeting;
+
